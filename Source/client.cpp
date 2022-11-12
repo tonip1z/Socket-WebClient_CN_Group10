228e5a0e42ca5c748fd2b55ba2d705298a562ebc
tonip1z <tonicp.1932@gmail.com>
2022-11-12 14:55:09 +0700

    added is_HTTP_URL() function

--- Source/client.cpp
+++ Source/client.cpp
@@ -5,6 +5,11 @@
 #include <WinSock2.h>
 #include <ws2tcpip.h>
 
+#include "client.h"
+
+//Note to compiler: if you're using g++ to compile this code please add "-lws2_32" after "g++ client.cpp [other files]"
+//For example: "g++ client.cpp -lws2_32"
+
 //ref to winsock2.h example code: https://learn.microsoft.com/en-us/windows/win32/winsock/complete-client-code
 //Link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
 #pragma comment (lib, "Ws2_32.lib")
@@ -27,7 +32,7 @@ int main(int argc, char* argv[])
     WSADATA wsaData;
 
     //sock_Connect is used for connecting to web servers
-    SOCKET sock_Connect;
+    SOCKET sock_Connect = INVALID_SOCKET; 
 
     //Create Internet address structure
     //learn more: https://learn.microsoft.com/en-us/windows/win32/api/ws2def/ns-ws2def-addrinfoa
@@ -50,12 +55,21 @@ int main(int argc, char* argv[])
 
     //Resolve the server address and port
     //Please make sure your IP Routing is enabled on your Windows IP Configuration (to check: type ipconfig /all in cmd)
+    //To enable IP Routing, see: https://www.wikihow.com/Enable-IP-Routing-on-Windows-10
     int getAddrInfo_Result = getaddrinfo(argv[1], PORT, &hints, &result);
     if (getAddrInfo_Result != 0) 
     {
         if (getAddrInfo_Result == 11001)
         {
             cout << "Host not found.\n";
+
+            //ref: https://superuser.com/questions/326403/can-you-get-a-reply-from-a-https-site-using-the-ping-command
+            if (is_HTTP_URL(argv[1]))
+            {
+                cout << "Cannot retrieve address from an URL. Please enter either a host-name or a host-ip.\n";
+                cout << "(E.g: example.com is a host name, while https://example.com/ is not)\n";
+            }
+
             WSACleanup();
             return 1;
         }
@@ -73,6 +87,26 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    cout << "Socket created successfully.\n";
+    //Establish connection
+    int connect_Result = connect(sock_Connect, result->ai_addr, (int)result->ai_addrlen);
+    if (connect_Result == SOCKET_ERROR)
+    {
+        cout << "Connection failed.\n";
+        closesocket(sock_Connect);
+        sock_Connect = INVALID_SOCKET;
+        return 1;
+    }
+
+    cout << result->ai_addr << "\n";
+
     return 0;
+}
+
+//check if entered host-name/host-ip is entered as a URL starting with "http:" or "https:"
+bool is_HTTP_URL(char* host_name)
+{
+    if (host_name[0] == 'h' && host_name[1] == 't' && host_name[2] == 't' && host_name[3] == 'p')
+        return true;
+    
+    return false;
 }
---
+++ Source/client.h
@@ -0,0 +1,5 @@
+#pragma once
+
+
+//support functions
+bool is_HTTP_URL(char* host_name);
